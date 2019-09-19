28f1ddc585ccd0fa8fa5ba44e9d598f06d14da6c
Markof <markof8788@gmail.com>
2019-09-19 16:54:18 +0200

    First version of Pong game

---
+++ Assets/Scripts/PaddleLeft.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleLeft : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            transform.Translate(0, 20 * Time.deltaTime, 0);
+
+        if (Input.GetKey("s"))
+            transform.Translate(0, -20 * Time.deltaTime, 0);
+    }
+}
